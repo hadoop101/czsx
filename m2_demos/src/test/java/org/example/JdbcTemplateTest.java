477133e26ee37a41e0d5351d14f5a3aa04ef3b2d
hadoop101 <hadoop101@126.com>
2020-06-24 09:29:08 +0800

    JdbcTemplate 完成查询 需要类与表的映射关系（列名与成员变量同名）

--- m2_demos/src/test/java/org/example/JdbcTemplateTest.java
+++ m2_demos/src/test/java/org/example/JdbcTemplateTest.java
@@ -2,14 +2,18 @@ package org.example;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.pool.DruidDataSourceFactory;
+import org.example.bean.User;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class JdbcTemplateTest {
@@ -41,6 +45,35 @@ public class JdbcTemplateTest {
     public void test03update(){
         System.out.println("---test03");
         jdbcTemplate.update("UPDATE tab_user SET username = ?,`password` = ? WHERE uid =?","baoqiang","1234",1003L);
+    }
+
+    @Test
+    public void test04select(){
+        System.out.println("---test04");
+        //1:编写 sql查询语句 SELECT * FROM tab_user WHERE uid = 1003;
+        //2:定义一个实体类，有10个变量，名字要跟列名一样。
+        //3:调用query
+        String sql = "SELECT * FROM tab_user WHERE uid = ?";
+        //查询一条记录，将该记录保存在一个对象的成员变量里面
+        //参1：是sql语句  参2：转换规则 参3：数值
+        RowMapper<User> mapper = new RowMapper<User>() {
+            //结果集表示的是查询的结果
+            @Override
+            public User mapRow(ResultSet resultSet, int i) throws SQLException {
+                //作映射或者叫对应
+                User user = new User();
+                user.setUid(resultSet.getLong("uid"));
+                user.setUsername(resultSet.getString("username"));
+                user.setPassword(resultSet.getString("password"));
+                //还有7个值与变量需要对应
+                return user;
+            }
+
+
+        };
+        User user=jdbcTemplate.queryForObject(sql,mapper,1001L);
+        System.out.println(user);
+
     }
     private DataSource getDruidDataSource() throws Exception {
         //1:读配置文件
